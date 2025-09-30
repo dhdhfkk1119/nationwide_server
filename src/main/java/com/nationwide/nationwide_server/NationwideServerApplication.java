4d425f549842daabbdc033282f010613dc046cbb
Leeseungmin <tmxjel77777@naver.com>
2025-09-30 14:45:32 +0900

    first commit

---
+++ src/main/java/com/nationwide/nationwide_server/NationwideServerApplication.java
@@ -0,0 +1,13 @@
+package com.nationwide.nationwide_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationwideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationwideServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nationwide/nationwide_server/NationwideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nationwide.nationwide_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationwideServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
