78d53ed9b625c75b4a84d3d022346f6cf480428d
Parkyeon11 <py@bestevertech.io>
2025-12-11 08:52:40 +0900

    swagger 추가

--- src/main/java/com/nationwide/nationwide_server/NationwideServerApplication.java
+++ src/main/java/com/nationwide/nationwide_server/NationwideServerApplication.java
@@ -1,7 +1,17 @@
 package com.nationwide.nationwide_server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Nation Wide - Server",
+                description = "백엔드 API 문서",
+                version = "1.0"
+        )
+)
 
 @SpringBootApplication
 public class NationwideServerApplication {
